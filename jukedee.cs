c3c5007995486324d868edf1c65686927b26d9cf
xbox <xboxsxeed@gmail.com>
2026-09-02 18:04:18 -0400

    Add files via upload

---
+++ jukedee.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace jukez {
+    
+    class dee {
+        
+        public static void Main(string[] args) {
+            
+            jukebro();
+            
+        }
+        static void jukebro() {
+            
+            string juke = "🍡";
+            string powerup = "🍄";
+            
+            Console.WriteLine("Juke Bros @xboxgunner420 " + juke);
+            Console.WriteLine("power Ups! " + powerup);
+            
+            
+        }
+    }
+    
+}
