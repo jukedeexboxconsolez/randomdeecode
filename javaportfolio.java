225ed5ef46c30222e706abf844e1752638f95b44
xbox <xboxsxeed@gmail.com>
2026-09-03 01:57:08 -0400

    Add files via upload

---
+++ javaportfolio.java
@@ -0,0 +1,37 @@
+public class java {
+    
+    public static void main(String[] args) {
+        
+        int interger ;
+        
+        char character;
+        
+        long data;
+        
+        double dart;
+        
+        String ammo = "🍡 ";
+        
+        System.out.print(ammo);
+        
+        float numberxz = 16.5f; 
+        
+        String hut = "🛖 ";
+        
+        String bento = "🍱";
+        
+        String shroom = "🍄";
+        
+        System.out.print(hut);
+        
+        int xbox = 10 * 10;
+        
+        System.out.print(xbox);
+        
+        char y = 'y';
+        
+        
+        
+    }
+    
+}
