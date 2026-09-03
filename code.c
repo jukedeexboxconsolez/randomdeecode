911b07da2031255066a131893a26202e00edfc78
xbox <xboxsxeed@gmail.com>
2026-09-03 00:38:41 -0400

    Add files via upload

---
+++ code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    
+    int jarva[] = {15, 20, 25, 30, 35, 40,};
+    
+    double java[] = {16.5, 56.8, 75.16, 10.6};
+    
+    
+}
---
+++ random.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+void computer(char comp[]) {
+    
+    char pc[] = "🖥️️";
+    
+    printf("%s\n", pc);
+    
+    printf("💽 ");
+    
+    printf("💿 ");
+    
+    
+    
+    
+}
+int addup(int symbol) {
+    
+    int sub = 5 * 5;
+
+
+}
+
+
+int main() {
+    
+    
+    char comp[] = "🖥️";
+    
+    computer(comp);
+    
+    #define answer 6 * 6
+    
+    printf("%d\n", answer);
+    
+
+    float low = 16.5;
+    
+    char voidstar[] = "💫";
+    
+    printf("%s\n", voidstar);
+    
+   
+    printf("%f\n", low);
+    
+    int symbol = 16 + 22;
+    
+    addup(symbol);
+    
+    
+    
+ 
+
+}
