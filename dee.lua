8591551ad84fd77707227fb9e90848469b93b5fe
xbox <xboxsxeed@gmail.com>
2026-09-03 14:09:52 -0400

    Add files via upload

---
+++ dee.lua
@@ -0,0 +1,22 @@
+mcdonaldschar = {
+    
+    grimace = "😈",
+    
+    dee = "🍡",
+    
+    shake = "🧋",
+    
+    elmo = "👹",
+    
+    ronald = "🤡"
+    
+    
+    
+}
+
+print(mcdonaldschar.grimace)
+print(mcdonaldschar.dee)
+print(mcdonaldschar.shake)
+print(mcdonaldschar.elmo)
+print(mcdonaldschar.ronald)
+
---
+++ goport.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+func main() {
+	
+     var variable int32 = 10
+     
+     fmt.Println(variable)
+     
+     var word string = "Hello World";
+     
+     fmt.Println(word)
+     
+     var floatx float64 = 152.16
+     
+     fmt.Println(floatx)
+     
+     var doublex float32 = 15.16
+     
+     fmt.Println(doublex)
+	
+}
