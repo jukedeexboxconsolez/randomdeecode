8b6e33c669271b67870f1ea7714f3100e02c24ad
xbox <xboxsxeed@gmail.com>
2026-09-03 12:43:06 -0400

    Add files via upload

---
+++ mariodee.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class java {
+    public static void main(String[] args) {
+      
+      int mario = 10;
+      
+      
+      
+      System.out.print(" Mario's Score is " + mario);
+      
+      float doublex = 16.5f; 
+      
+      double hex = 168.76;
+      
+      
+      
+      mushroom();
+      
+      String hut = "🛖";
+      
+      
+    }
+    static void mushroom() {
+        
+        System.out.print(" 🍄 ");
+        System.out.print("🍄 ");
+        System.out.print("🍄 ");
+        System.out.print("🍄 ");
+        System.out.print("🍄 ");
+    }
+}
