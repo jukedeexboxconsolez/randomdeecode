5820bd2a6512a5d4deb1969f6d1a260f02e23189
xbox <xboxsxeed@gmail.com>
2026-09-03 19:46:54 -0400

    Add files via upload

---
+++ FireGunLong.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      long troll = 16462828L;
+      
+      System.out.print(troll);
+      
+      FireGun();
+      FireGun();
+      FireGun();
+      FireGun();
+      
+    }
+    static void FireGun() {
+        
+        System.out.print(" 💥");
+        System.out.print("💥");
+        System.out.print("💥");
+        System.out.print("💥");
+        System.out.print("💥");
+        System.out.print("💥");
+        System.out.print("💥");
+        
+    }
+}
