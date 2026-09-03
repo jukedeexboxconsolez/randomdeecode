3b34aebab999ca20779610de07d07529d331cda2
xbox <xboxsxeed@gmail.com>
2026-09-03 12:42:30 -0400

    Add files via upload

---
+++ xboxotoole.lua
@@ -0,0 +1,24 @@
+-- xbox o toole song code thing
+name = "xbox"
+xbox_on = true
+if xbox_on then
+  print(name .. " : im not turning my xbox off i am xbox o toole ")
+    guards = "hey turn that xbox off "
+    xbox_says = "no"
+    guards2 = "turn it off i said"
+    xbox_says2 = "no!"
+    -- runs off 
+   guard3 = ("-- ahh we'll never turn it off -- not in these powers")
+   
+  print(guards)
+    print(xbox_says)
+    print(guards2)
+    print(xbox_says2)
+ print(guard3)
+afterstatement = "i aint turned my xbox off im still xbox o toole " .. name
+  print(afterstatement)
+else
+  print("...")
+  end
+
+-- Something i coded for the funny
