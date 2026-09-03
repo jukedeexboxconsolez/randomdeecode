30fe4140a7f90860c81af8ff8f2d2cdb2eb60ef2
xbox <xboxsxeed@gmail.com>
2026-09-03 17:05:50 -0400

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    
+    // Game Or Something
+    
+ const string keybindcontrol = "WASD";
+    
+    cout << keybindcontrol;
+    
+    // Locked WASD Keybind 
+    
+    int selection = 8;
+    
+    cout << " Selections : " << selection;
+    
+    string character =  "🤡";
+    
+    string character2 = "👹";
+    
+    string character3 = "😈";
+    
+    // Character Selections 
+    
+    cout << " Select Character 1': " << character; 
+    
+    cout << " Select Character 2 : " << character2;
+    
+    cout << " Select Character 3 : " << character3;
+    
+    
+    
+}
