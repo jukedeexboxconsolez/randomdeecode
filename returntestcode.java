0d04e75ce3aabb804ad3673bfb7a45c69a4084c4
xbox <xboxsxeed@gmail.com>
2026-09-03 13:27:53 -0400

    Add files via upload

---
+++ returntestcode.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class javaclass {
+    public static void main(String[] args) {
+     
+         System.out.print(takeaway(5));
+      
+    }
+    static int takeaway(int coins) {
+        
+        int coin = coins + coins;
+        
+        return coin;
+        
+        
+        
+    }
+    
+    
+    
+}
