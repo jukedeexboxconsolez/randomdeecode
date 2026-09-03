90430a225c2f222afa4ded4b0a0c645b4dec7178
xbox <xboxsxeed@gmail.com>
2026-09-02 21:48:28 -0400

    Add files via upload

---
+++ randomdeefunction.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+void mushroom(char emojiswirlz[]) {
+    
+    printf("🍄");
+    printf("🍄");
+    printf("🍄");
+    printf("🍄");
+    printf("🍄");
+    printf("🍄");
+    printf("🍄");
+    printf("🍄");
+    
+    char mushroomemoji[] = "🍄";
+    
+    printf(mushroomemoji);
+    
+}
+
+int main()
+{
+    
+    char emojiswirlz[] = "😵‍💫";
+    
+    printf("%s\n", emojiswirlz);
+    
+    mushroom(emojiswirlz);
+    
+
+    
+}
