46f597e4068913df618a5b8fd41a7dddc44ce854
xbox <xboxsxeed@gmail.com>
2026-09-04 16:40:09 -0400

    Add files via upload

---
+++ userinputport.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    
+    int tools;
+    
+    char tool[] = " 🛠 ️";
+   
+    printf("Enter Your Tools Amount %s\n", tool);
+    
+    scanf("%d", tools);
+    
+    
+}
