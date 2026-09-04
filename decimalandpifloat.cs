989754c61d51b9ade1cf264d471f81599694404b
xbox <xboxsxeed@gmail.com>
2026-09-04 17:23:41 -0400

    Add files via upload

---
+++ decimalandpifloat.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace pi
+{
+	public class decimalx
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			decimal y = 10.5M;
+			
+			Console.WriteLine(y);
+			
+			float pi = 3.14f;
+			
+			Console.WriteLine(pi);
+			
+			
+		}
+	}
+}
