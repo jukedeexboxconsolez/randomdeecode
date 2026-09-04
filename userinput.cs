8f87d28181cb1618a58e3c17e2744175bebb00a8
xbox <xboxsxeed@gmail.com>
2026-09-04 18:43:10 -0400

    Add files via upload

---
+++ userinput.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter Your Name : ");
+			
+			string name = Console.ReadLine();
+			
+			Console.WriteLine("Hello " + name);
+			
+			
+			
+		}
+	}
+}
