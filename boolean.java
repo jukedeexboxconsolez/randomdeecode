cbb8f5bb641b60efe75f736ba0392f1f1ba76643
xbox <xboxsxeed@gmail.com>
2026-09-04 13:08:52 -0400

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class booljava {
+    public static void main(String[] args) {
+
+      int x = 10;
+      
+      boolean z = true;
+      
+      boolean v = false; 
+      
+      System.out.print(z);
+      System.out.print(v);
+      
+      if (v) {
+          
+          System.out.print("V Is Considered False And Will Not Print ");
+          
+          } else if (v == true) {
+              
+              
+              System.out.print("V Is Considered False And Will Print");
+              
+              } else {
+                  
+                  System.out.print("Thats It");
+                  
+              }
+              
+              
+          
+          
+          
+      }
+
+
+    }
