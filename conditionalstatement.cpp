5059aa20b5645779aab0264705d48a08f3e3b18c
xbox <xboxsxeed@gmail.com>
2026-09-05 22:18:24 -0400

    Add files via upload

---
+++ conditionalstatement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    
+    bool marked = true;
+    
+    if (marked) {
+        
+        cout << "The Condition Is Marked";
+        
+    } else {
+        
+        cout << "Nothing";
+        
+    }
+    
+    
+}
