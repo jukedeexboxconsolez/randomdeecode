15596adfba3d90d41da608d435615fdaeb0aa145
xbox <xboxsxeed@gmail.com>
2026-09-05 22:16:14 -0400

    Add files via upload

---
+++ mushroomboolean.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class mushroomboolean {
+    public static void main(String[] args) {
+      
+      Boolean mushroom = true;
+      
+      if (mushroom) {
+      
+          
+          System.out.print("🍄🍄🍄🍄🍄");
+          
+          } else if (mushroom == false) {
+              
+              System.out.print("⚫️");
+              
+              
+              } else {
+                  
+                  System.out.print("🍄🍄🍄🍄🍄");
+                  
+              }
+              
+          
+          
+      
+      
+      
+    }
+}
