55c971435ec1576bd263ffe1d6ac01219b56205d
xbox <xboxsxeed@gmail.com>
2026-09-06 15:39:38 -0400

    Add files via upload

---
+++ toolsinput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    
+    char tool[] = "🛠️";
+    
+    int tools;
+    
+    printf("Enter The Amount Of Tools You Need ");
+    
+    scanf("%d ", &tools);
+    
+}
