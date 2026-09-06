1518eb6adff57b911cfdbbefefa8b5be5766c0c5
xbox <xboxsxeed@gmail.com>
2026-09-05 22:09:53 -0400

    Add files via upload

---
+++ locked.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace market
+{
+	public class locked
+	{
+		public static void Main(string[] args)
+		{
+		    
+		    Console.WriteLine(chain(5));
+			
+			const string locked = "🔐";
+			
+			Console.WriteLine(locked);
+			
+			bool mart = true;
+			
+			
+			if (mart) {
+			    
+			    Console.WriteLine("Mart Is A Valid Supermarket");
+			    
+			    
+			    
+			}
+			 
+			    
+			}
+			static int chain(int chainz) {
+			    
+			    int answer = chainz * chainz;
+			    
+			    return answer;
+			
+		}
+	}
+}
---
+++ yup.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class javaclassed {
+    public static void main(String[] args) {
+      
+      
+      int returned = 14;
+      
+      int xturn = 18;
+      
+      int factz = xturn + returned;
+      
+      System.out.print(factz);
+      
+      
+    }
+}
