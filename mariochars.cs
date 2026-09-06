c987f507a2db1b8193167f91a0f857e04592cd87
xbox <xboxsxeed@gmail.com>
2026-09-05 22:45:16 -0400

    Add files via upload

---
+++ mariochars.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace mariocharacters
+{
+	public class array
+	{
+		public static void Main(string[] args)
+		{
+			
+			string[] mariochars = {"", "Mario", "Luigi", "Toad", "Princess Peach", "Goomba", "Bowser", "Koopa",};
+			
+			Console.WriteLine(mariochars[1]);
+			
+			
+		}
+	}
+}
