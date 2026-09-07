317e3b86b0f53a364867a0b51751809baa225b3f
xbox <xboxsxeed@gmail.com>
2026-09-06 20:47:36 -0400

    Add files via upload

---
+++ MachineGun.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    
+    int MachineGun = 1000; // Machine Gun 🔫
+    
+    switch(MachineGun) {
+        
+        case 1: 
+        
+        cout << "1 Bullet";
+        
+        break;
+        
+        case 2: 
+        
+        cout << "2 Bullets";
+        
+        break;
+        
+        case 3: 
+        
+        cout << "3 Bullets";
+        
+        break;
+        
+        case 4: 
+        
+        cout << "4 Bullets";
+        
+        break;
+        
+        case 5: 
+        
+        cout << "5 Bullets";
+        
+        break;
+        
+        case 6:
+        
+        cout << "6 Bullets";
+        
+        case 7:
+        
+        cout << "7 Bullets";
+        
+        break;
+        
+            
+        
+    }
+    
+    
+}
