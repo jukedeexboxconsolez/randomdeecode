0d5987dfb16635b0c1a33fa98d304d43da02204e
xbox <xboxsxeed@gmail.com>
2026-09-06 22:37:48 -0400

    Add files via upload

---
+++ mathfunction.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int mario = 10;
+    
+    mario = sqrt(mario);
+    
+    printf("%d", mario);
+    
+    mario = pow(mario, 8);
+    
+    float juice = 3.14f;
+    
+    juice = round(juice);
+    
+    printf("%f", juice);
+    
+    juice = floor(juice);
+    
+    printf("%f", juice);
+    
+    juice = log(juice);
+    
+    printf("%f", juice);
+    
+    juice = ceil(juice);
+    
+    printf("%f", juice);
+    
+    
+    
+}
